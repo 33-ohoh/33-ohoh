3942740b33979e2046645ef71cca9b3f4acff543
an seungji <ansj8777@gmail.com>
2024-01-22 18:27:56 +0900

    Feat: lexical 텍스트 에디터 설치 및 테스트 #6

--- apps/web/app/log/page.tsx
+++ apps/web/app/log/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import LexicalEditor from "@repo/ui/lexicalEditor";
 
 const Log = () => {
-  return <div>Log</div>;
+  return (
+    <div>
+      <h1>Your Page</h1>
+      <LexicalEditor />
+    </div>
+  );
 };
 
 export default Log;
---
+++ packages/ui/src/lexicalEditor.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+function onError(error: Error): void {
+  console.error(error);
+}
+
+const LexicalEditor = () => {
+  const initialConfig = {
+    namespace: "MyEditor",
+    onError,
+  };
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
+  );
+};
+
+export default LexicalEditor;
