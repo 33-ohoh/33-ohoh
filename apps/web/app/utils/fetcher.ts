0424c8d648491dd6c2bcc533f08fe084699acaa0
wantop1 <65522153+wantop1@users.noreply.github.com>
2024-01-23 14:03:18 +0900

    [Feat] swr 설치 및 api util 구현 #8 (#9)
    * Feat: swr 설치
    
    * Chore: lint에 api url 환경변수 추가
    
    * Feat: 테스트용 Log type 추가
    
    * Feat: fetcher 유틸 추가
    
    * Feat: log api 추가
    
    * Feat: useLogs 추가

---
+++ apps/web/app/utils/fetcher.ts
@@ -0,0 +1,29 @@
+interface FetcherOptions {
+  method?: string;
+  headers?: Record<string, string>;
+  body?: string;
+}
+
+const fetcher = async <T>(
+  url: string,
+  options: FetcherOptions = {},
+): Promise<T | null> => {
+  const response = await fetch(url, {
+    method: options.method || "GET",
+    headers: options.headers || {},
+    body: options.body,
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  if (response.status === 204) {
+    return null;
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export default fetcher;
