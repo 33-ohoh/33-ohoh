f56fb9189eecfacd83e939cd6819c05426868937
an seungji <ansj8777@gmail.com>
2024-01-23 15:22:05 +0900

    Feat: Quill 텍스트 에디터 컴포넌트 구성 및 lexical 텍스트 에디터 제거 #6

--- apps/web/app/log/page.tsx
+++ apps/web/app/log/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import LexicalEditor from "@repo/ui/lexicalEditor";
 import QuillEditor from "@repo/ui/quillEditor";
 
 const Log = () => {
   return (
     <div>
       <h1>Your Page</h1>
-      {/* <LexicalEditor /> */}
       <QuillEditor />
     </div>
   );
--- packages/ui/src/quillEditor.tsx
+++ packages/ui/src/quillEditor.tsx
@@ -1,34 +1,71 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import Quill from "quill";
-import "quill/dist/quill.snow.css";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const QuillEditor = () => {
-  const editorRef = useRef(null);
-
-  useEffect(() => {
-    if (editorRef.current) {
-      new Quill(editorRef.current, {
-        theme: "snow",
-        modules: {
-          toolbar: [
-            [{ header: [1, 2, false] }],
-            ["bold", "italic", "underline", "strike", "blockquote"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["link", "image", "code-block"],
-            ["clean"],
-          ],
-        },
-      });
-    }
-  }, []);
+  const [text, setText] = useState("");
+  const handleChange = (
+    content: string,
+    delta: any,
+    source: string,
+    editor: any,
+  ): void => {
+    setText(editor.getHTML()); // rich text
+  };
 
   return (
-    <div className="text-editor">
-      <div ref={editorRef} />
+    <div>
+      <ReactQuill
+        value={text}
+        onChange={handleChange}
+        modules={QuillEditor.modules}
+        formats={QuillEditor.formats}
+        bounds={".app"}
+        placeholder="내용을 입력하세요."
+      />
     </div>
   );
 };
+
+QuillEditor.modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video", "code-block"],
+    [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+    ["clean"],
+  ],
+  clipboard: {
+    // Toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+
+QuillEditor.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "code-block",
+];
 
 export default QuillEditor;
