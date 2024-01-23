7e75340ef70ac5c9be2b6816cbe6df3c7c615583
ASJ <ansj8777@gmail.com>
2024-01-23 15:43:42 +0900

    [Feat] 텍스트 에디터 컴포넌트 구성 #6 (#11)
    * Feat: lexical 텍스트 에디터 설치 및 테스트 #6
    
    * Feat: Quill 텍스트 에디터 설치 및 테스트 #6
    
    * Feat: Quill 텍스트 에디터 컴포넌트 구성 및 lexical 텍스트 에디터 제거 #6
    
    * Chore: ui 내부 package.json 내부 lexicalEditor export문 제거

--- apps/web/app/log/page.tsx
+++ apps/web/app/log/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import QuillEditor from "@repo/ui/quillEditor";
 
 const Log = () => {
-  return <div>Log</div>;
+  return (
+    <div>
+      <h1>Your Page</h1>
+      <QuillEditor />
+    </div>
+  );
 };
 
 export default Log;
---
+++ packages/ui/src/quillEditor.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const QuillEditor = () => {
+  const [text, setText] = useState("");
+  const handleChange = (
+    content: string,
+    delta: any,
+    source: string,
+    editor: any,
+  ): void => {
+    setText(editor.getHTML()); // rich text
+  };
+
+  return (
+    <div>
+      <ReactQuill
+        value={text}
+        onChange={handleChange}
+        modules={QuillEditor.modules}
+        formats={QuillEditor.formats}
+        bounds={".app"}
+        placeholder="내용을 입력하세요."
+      />
+    </div>
+  );
+};
+
+QuillEditor.modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video", "code-block"],
+    [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+    ["clean"],
+  ],
+  clipboard: {
+    // Toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+
+QuillEditor.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "code-block",
+];
+
+export default QuillEditor;
