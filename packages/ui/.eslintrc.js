42b3f092da80ab30c57419a3666f8e78a5a168db
Hyein Jung <penguin636@naver.com>
2024-01-24 13:55:08 +0900

    Feat: ui폴더에 stylex 설치

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -8,4 +8,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
+  plugins: ["@stylexjs"],
+  rules: {
+    "@stylexjs/valid-styles": ["error", { ...options }],
+  },
 };
