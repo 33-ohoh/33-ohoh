d724bd8ba6050057b79bea14ecda6f955f3dacf7
Minji Kim <vvzmsqufvv@gmail.com>
2024-01-25 16:57:01 +0900

    [Feat] 마이페이지 사이드 및 관심로그 컴포넌트 구성 (#16)
    * Fix: import bug
    
    * Remove: delete unused files
    
    * Feat: create aside section component
    
    * Feat: create interest main section component
    
    * Feat: add UI to package.json

--- apps/web/app/mypage/page.tsx
+++ apps/web/app/mypage/page.tsx
@@ -1,5 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import * as stylex from "@stylexjs/stylex";
+import AsideContainer from "@repo/ui/asideContainer";
+import MainContainer from "@repo/ui/mainContainer";
+
 const Mypage = () => {
-  return <div>Mypage</div>;
+  // TODO:: 방문자 수 증가 구현
+  const [visiting, setVisiting] = useState<number>(122);
+  const [totalVisiting, setTotalVisiting] = useState<number>(12240);
+
+  return (
+    <div {...stylex.props(styles.layout)}>
+      <h2 {...stylex.props(styles.pageTitle)}>마이페이지</h2>
+      <div {...stylex.props(styles.visitingStatus)}>
+        <strong>
+          Today <span>{visiting}</span>
+        </strong>
+        <div>⎪</div>
+        <strong>
+          Total <span>{totalVisiting}</span>
+        </strong>
+      </div>
+      <div {...stylex.props(styles.containerLayout)}>
+        <AsideContainer />
+        <MainContainer />
+      </div>
+    </div>
+  );
 };
 
 export default Mypage;
+
+const styles = stylex.create({
+  layout: {
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  containerLayout: {
+    display: "flex",
+    gap: "50px",
+  },
+
+  pageTitle: {
+    fontSize: "28px",
+    marginTop: "188px",
+    marginBottom: "75px",
+  },
+
+  visitingStatus: {
+    display: "flex",
+    fontSize: "18px",
+    marginBottom: "15px",
+  },
+});
---
+++ packages/ui/src/mypage/interestLog.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InterestLog = () => {
+  return <div>InterestLog</div>;
+};
+
+export default InterestLog;
