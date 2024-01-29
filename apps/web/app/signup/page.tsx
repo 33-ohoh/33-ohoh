cd22f842ca52853f226238ab90a1220abfcc8537
wantop1 <wantop@jbnu.ac.kr>
2024-01-29 17:00:31 +0900

    Feat: 회원가입 페이지 로직 구현

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import InterestAndPrivacyForm from "../../components/signup/InterestAndPrivacyForm";
+import SignupForm from "../../components/signup/SignupForm";
+import SignUpSuccess from "../../components/signup/SignUpSuccess";
+import SignupTerm from "../../components/signup/SignupTerm";
 
-const Page = () => {
-  return <div>Signup</div>;
+import { useState } from "react";
+import withAuth from "../../utils/withAuth";
+
+const Signup = () => {
+  const [step, setStep] = useState(1);
+  let currentPage = <SignupTerm setStep={setStep} />;
+
+  switch (step) {
+    case 1:
+      currentPage = <SignupTerm setStep={setStep} />;
+      break;
+    case 2:
+      currentPage = <SignupForm setStep={setStep} />;
+      break;
+    case 3:
+      currentPage = <InterestAndPrivacyForm setStep={setStep} />;
+      break;
+    case 4:
+      currentPage = <SignUpSuccess />;
+      break;
+    default:
+      currentPage = <SignupTerm setStep={setStep} />;
+      break;
+  }
+
+  return (
+    <>
+      <h1>signup</h1>
+      {step !== 4 && <div>stepper</div>}
+      {currentPage}
+    </>
+  );
 };
 
-export default Page;
+export default withAuth(Signup);
