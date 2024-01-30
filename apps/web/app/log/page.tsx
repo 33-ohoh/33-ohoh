c2970d45e96b8357182a32b52917d2d3b576f210
an seungji <ansj8777@gmail.com>
2024-01-30 17:42:11 +0900

    Feat: log 작성 페이지 마크업 수정 및 레이아웃 구성 #12

--- apps/web/app/log/page.tsx
+++ apps/web/app/log/page.tsx
@@ -3,32 +3,133 @@
 import QuillEditor from "@repo/ui/quillEditor";
 import { useState } from "react";
 import TemplateModal from "../../components/log/templateModal";
+import Image from "next/image";
 
 const Page = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const [showAllTags, setShowAllTags] = useState<boolean>(false);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  //   const [images, setImages] = useState([]);
+  const [thumbnail, setThumbnail] = useState("");
+
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
+  const toggleTags = () => setShowAllTags(!showAllTags);
+
+  const tags = [
+    "전체",
+    "서버/백엔드 개발자",
+    "웹 풀스택 개발자",
+    "안드로이드 개발자",
+    "IOS 개발자",
+    "크로스플랫폼 앱개발자",
+    "게임 클라이언트 개발자",
+    "DBA",
+    "빅데이터 엔지니어",
+    "인공지능/머신러닝",
+    "프론트엔드 개발자",
+    "개발 PM",
+    "게임 서버 개발자",
+    "블록체인",
+    "QA 엔지니어",
+    "웹퍼블리셔",
+  ];
+
+  const selectTag = (tag: string): void => {
+    setSelectedTags((prevSelectedTags) => {
+      if (prevSelectedTags.includes(tag)) {
+        // 태그가 이미 선택된 경우 제거
+        return prevSelectedTags.filter((t) => t !== tag);
+      } else {
+        // 태그가 선택되지 않은 경우 추가
+        return [...prevSelectedTags, tag];
+      }
+    });
+  };
+
+  const recommendedThumbnails = [
+    "/recommendedThumbnail.png",
+    "/recommendedThumbnail.png",
+    "/recommendedThumbnail.png",
+  ];
+
+  const handleImageUpload = () => {};
+
+  // 썸네일 선택 핸들러
+  const selectThumbnail = (image: string) => {
+    setThumbnail(image);
+  };
+
   return (
     <section className="mx-auto w-[1185px]">
-      <div className="flex justify-between items-center mt-regular1 mb-small1">
+      <div className="flex justify-between items-center mt-regular1 mb-small1 relative">
         <h1 className="display3 text-neutral80">게시글 작성</h1>
-        <div className="flex items-center">
-          <p className="mr-extraSmall2 text-neutral40 subhead1">
-            자동 저장 21:10:27
-          </p>
+        <div className="flex items-center gap-[23px]">
+          <button
+            onClick={openModal}
+            className="bg-primary50 p-[9px] w-[138px] h-[40px] rounded-radius5 text-white subhead1"
+          >
+            템플릿 추가
+          </button>
+
           <button className="bg-neutral40 p-[9px] w-[138px] h-[40px] rounded-radius5 text-white subhead1">
             임시저장 | 50
           </button>
         </div>
-      </div>
-      <div className="h-[800px]">
-        <button onClick={openModal}>템플릿 추가</button>
-        <QuillEditor />
         <TemplateModal isOpen={isModalOpen} onClose={closeModal}>
           <h2 className="text-lg font-bold mb-4">템플릿 선택</h2>
         </TemplateModal>
       </div>
+      <div>
+        <QuillEditor />
+      </div>
       <div className="flex flex-col gap-[15px] mt-small1">
+        <div className="flex items-center">
+          <span className="body1M">태그</span>
+          <button onClick={toggleTags}>
+            {showAllTags ? "간략히" : "더보기"}
+          </button>
+          <ul className="flex flex-wrap">
+            {(showAllTags ? tags : selectedTags).map((tag, index) => (
+              <li key={index} className="mr-2 mb-2">
+                <button
+                  className={`px-3 py-1 border ${selectedTags.includes(tag) ? "border-blue-500 bg-blue-100" : "border-gray-300"}`}
+                  onClick={() => selectTag(tag)}
+                >
+                  {tag}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div className="flex justify-between items-center mb-3">
+            <h2 className="text-lg font-bold">썸네일 추천</h2>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+              onClick={handleImageUpload}
+            >
+              직접 업로드 하기
+            </button>
+          </div>
+          <div className="grid grid-cols-3">
+            {recommendedThumbnails.map((image, index) => (
+              <div
+                key={index}
+                className={`border-2 p-4 rounded-lg ${thumbnail === image ? "border-blue-500" : "border-transparent"}`}
+                onClick={() => selectThumbnail(image)}
+              >
+                <Image
+                  src={image}
+                  alt={`Thumbnail ${index}`}
+                  width={300}
+                  height={300}
+                  layout="responsive"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
         <div className="flex items-center">
           <span className="body1M w-[132px]">시리즈 설정</span>
           <select name="series" id="series">
--- apps/web/components/log/templateModal.tsx
+++ apps/web/components/log/templateModal.tsx
@@ -14,7 +14,7 @@ const TemplateModal: React.FC<TemplateModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="p-4 rounded-lg">
+    <div className="p-4 rounded-lg absolute right-0 top-[42px] border border-solid border-cyan-400 w-[200px] h-[300px] bg-slate-400">
       {children}
       <button onClick={onClose}>Close</button>
     </div>
