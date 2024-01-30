ea26bde3e5343181db3f12316db990ef7c999fa4
minzyee <vvzmsqufvv@gmail.com>
2024-01-30 16:11:56 +0900

    Feat: 마이페이지 메인컨테이더 마크업

--- apps/web/components/mypage/LogCard.tsx
+++ apps/web/components/mypage/LogCard.tsx
@@ -1,209 +1,85 @@
-import * as stylex from "@stylexjs/stylex";
-import {
-  bodyStyle,
-  colors,
-  margins,
-  radius,
-  titleStyle,
-} from "@repo/ui/designToken";
 import Image from "next/image";
+import {
+  Bookmark,
+  Comment,
+  Heart,
+  Viewer,
+} from "../../../../packages/ui/src/svg/ProviderIcon";
 
-// TODO:: 더미데이터 포켓베이스에 넣어두고, 그거 불러오는 코드 작성하기
-const LogCard = () => {
-  const logCardData = [
-    {
-      id: "1",
-      username: "홍길동",
-      job: "PW, developer",
-      bookmark: true,
-      title: "개발자가 되고 싶은 이유",
-      context:
-        "개발자가 되고 싶은 이유는 이건 돈을 많이 벌기 때문이라고 정말 생각하세요,,,?",
-      viewer: "2.1K",
-      like: "680",
-    },
-    {
-      id: "2",
-      username: "홍길동",
-      job: "PW, developer",
-      bookmark: true,
-      title: "개발자가 되고 싶은 이유",
-      context: "개발자가 되고 싶은 이유는 이건 돈을 많이 벌기 때문...",
-      viewer: "2.1K",
-      like: "680",
-    },
-    {
-      id: "3",
-      username: "길동이",
-      job: "PW, developer",
-      bookmark: true,
-      title: "개발자가 되고 싶은 이유",
-      context: "개발자가 되고 싶은 이유는 이건 돈을 많이 벌기 때문...",
-      viewer: "2.1K",
-      like: "680",
-    },
-    {
-      id: "4",
-      username: "길동이",
-      job: "PW, developer",
-      bookmark: true,
-      title: "개발자가 되고 싶은 이유",
-      context: "개발자가 되고 싶은 이유는 이건 돈을 많이 벌기 때문...",
-      viewer: "2.1K",
-      like: "680",
-    },
-    {
-      id: "5",
-      username: "길동이",
-      job: "PW, developer",
-      bookmark: true,
-      title: "개발자가 되고 싶은 이유",
-      context: "개발자가 되고 싶은 이유는 이건 돈을 많이 벌기 때문...",
-      viewer: "2.1K",
-      like: "680",
-    },
-  ];
+import userProfile from "../../../../packages/ui/src/public/user-profile.svg";
 
-  let logCardItem = logCardData.map((item) => {
-    return (
-      <div key={item.id + item.title} {...stylex.props(styles.cardTemplate)}>
-        <div {...stylex.props(styles.thumbnailWrap)}>
-          <div {...stylex.props(styles.userinfoWrap)}>
-            <Image src={""} alt={"img"} {...stylex.props(styles.profile)} />
-            <div {...stylex.props(styles.profileWrap)}>
-              <strong {...stylex.props(bodyStyle.body4M)}>
-                {item.username}
-              </strong>
-              <span {...stylex.props(bodyStyle.body6R)}>{item.job}</span>
-            </div>
-          </div>
-          <button {...stylex.props(styles.badge)}>
-            <Image src="" alt="" />
-          </button>
-        </div>
+interface LogCardProps {
+  id: string;
+  title: string;
+  thumbnail: string;
+  username: string;
+  job: string;
+  content: string;
+  viewer: number;
+  like: number;
+  comment: number;
+}
 
-        <div {...stylex.props(styles.contextWrap)}>
-          <h4 {...stylex.props(styles.title)}>{item.title}</h4>
-          <p {...stylex.props(styles.context)}>{item.context}</p>
-          <div {...stylex.props(styles.viewerLikeWrap)}>
-            <div {...stylex.props(styles.viewerLike)}>
-              <Image src="" alt="" />
-              <span>{item.viewer}</span>
-            </div>
-            <div {...stylex.props(styles.viewerLike)}>
-              <Image src="" alt="" />
-              <span>{item.like}</span>
-            </div>
+const LogCard = async ({ log }: { log: LogCardProps }) => {
+  const {
+    id,
+    title,
+    thumbnail,
+    username,
+    job,
+    content,
+    viewer,
+    like,
+    comment,
+  } = log || {};
+  return (
+    <li
+      key={id + title}
+      className="w-[320px] h-[302px] rounded-[10px] overflow-hidden border border-solid border-neutral20"
+    >
+      <div className="bg-gray-100 h-[194px] p-[16px]">
+        <div className="flex items-center gap-[5px]">
+          <Image
+            src={userProfile}
+            alt={"img"}
+            width={42}
+            height={42}
+            className="rounded-full"
+          />
+          <div className="flex flex-col gap-[5px]">
+            <strong className="text-[14px]">{username}</strong>
+            <span className="text-[10px]">{job}</span>
           </div>
         </div>
       </div>
-    );
-  });
 
-  return (
-    <li>
-      <a href="#" {...stylex.props(styles.layout)}>
-        {logCardItem}
-      </a>
+      <div className="flex flex-col justify-between bg-white h-[108px] py-[15px] px-[20px]">
+        <div className="flex">
+          <h4 className="text-[18px] font-bold mb-extraSmall5">{title}</h4>
+          <button>
+            <Bookmark fill="#000000" />
+          </button>
+        </div>
+        <p className="text-[14px] font-normal text-neutral50 text-ellipsis overflow-hidden whitespace-nowrap mb-extraSmall4">
+          {content}
+        </p>
+        <div className="flex text-[10px] text-neutral50 gap-[5px] pb-[10px]">
+          <div className="flex gap-[2px] items-center">
+            <Viewer />
+            <span>{viewer}</span>
+          </div>
+          <div className="flex gap-[2px] items-center">
+            <Heart fill="#EE5C5C" />
+            <span>{like}</span>
+          </div>
+          <div className="flex gap-[2px] items-center">
+            <Comment />
+            <span>{comment}</span>
+          </div>
+        </div>
+      </div>
     </li>
   );
 };
 
 export default LogCard;
-
-const styles = stylex.create({
-  layout: {
-    display: "flex",
-    width: "695px",
-    flexWrap: "wrap",
-    gap: "50px",
-    textDecoration: "none",
-  },
-
-  cardTemplate: {
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: colors.primary50,
-    borderRadius: radius.radius15,
-    width: "320px",
-    height: "300px",
-    overflow: "hidden",
-    position: "relative",
-    cursor: "pointer",
-  },
-
-  // 전체 색상
-  thumbnailWrap: {
-    height: "204px",
-    padding: "16px 22px",
-    background: colors.primary10,
-    display: "flex",
-    justifyContent: "space-between",
-    position: "relative",
-  },
-
-  userinfoWrap: {
-    display: "flex",
-    gap: "5px",
-    alignItems: "center",
-    height: "32px",
-  },
-
-  profileWrap: {
-    display: "flex",
-    flexDirection: "column",
-    gap: "-2px",
-  },
-
-  profile: {
-    background: colors.primary90,
-    width: "32px",
-    height: "32px",
-    borderRadius: radius.radius100,
-    color: colors.white,
-  },
-
-  info: {
-    color: colors.white,
-  },
-
-  badge: {
-    position: "absolute",
-    width: "23px",
-    height: "30px",
-    top: "16px",
-    right: "16px",
-  },
-
-  contextWrap: {
-    height: "96px",
-    padding: "16px 20px",
-  },
-
-  title: {
-    fontSize: titleStyle.headline2,
-    marginBottom: margins.extraSmall5,
-  },
-
-  context: {
-    fontSize: "12px",
-    color: colors.neutral50,
-    marginBottom: "10px",
-    whiteSpace: "nowrap",
-    wordBreak: "break-all",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-  },
-
-  viewerLikeWrap: {
-    display: "flex",
-    gap: "10px",
-  },
-
-  viewerLike: {
-    fontSize: bodyStyle.body6R,
-    color: colors.neutral50,
-    alignItems: "center",
-    gap: "5px",
-  },
-});
--- apps/web/components/mypage/MainContainer.tsx
+++ apps/web/components/mypage/MainContainer.tsx
@@ -1,50 +1,36 @@
-import * as stylex from "@stylexjs/stylex";
-import LogCard from "./logCard";
+import LogCard from "./LogCard";
 
-const MainContainer = () => {
-  const interestLogChip = [
+const MainContainer = async () => {
+  const favoriteLogChip = [
     "전체",
     "서버/밴엔드 개발자",
     "웹 풀스택 개발자",
     "안드로이드 개발자",
   ];
 
+  const logs = [{}, {}, {}];
+
   return (
     <main>
-      <h3 {...stylex.props(styles.subTitle)}>관심로그</h3>
-      <div {...stylex.props(styles.buttonWrap)}>
-        {interestLogChip.map((logChip) => (
-          <button type="button" {...stylex.props(styles.chipButton)}>
+      {/* <h3 className=" text-[28px] font-bold mb-extraSmall5">대표로그</h3> */}
+      <div className="flex gap-[10px] mb-small1">
+        {favoriteLogChip.map((logChip: any, index: any) => (
+          <button
+            key={index}
+            type="button"
+            className={`border border-solid rounded-radius100 py-[10px] px-[22px]`}
+          >
             {logChip}
           </button>
         ))}
       </div>
-      <ul>
-        <LogCard />
+      <ul className="flex flex-wrap gap-[50px] w-[694px] h-[1006px]">
+        {logs.map((log: any) => {
+          return <LogCard key={log.id} log={log} />;
+        })}
       </ul>
     </main>
   );
 };
 
 export default MainContainer;
-
-const styles = stylex.create({
-  subTitle: {
-    marginBottom: "25px",
-  },
-
-  buttonWrap: {
-    display: "flex",
-    gap: "10px",
-    marginBottom: "50px",
-  },
-
-  chipButton: {
-    padding: "10px 22px",
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: "#196AFF",
-    borderRadius: "100px",
-    background: "white",
-  },
-});
