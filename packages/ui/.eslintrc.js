0edf1fa041ced30a2b2c73d9dd49402c133be912
minzyee <vvzmsqufvv@gmail.com>
2024-01-30 17:32:08 +0900

    Style: add png image

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,15 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.lint.json",
-    tsconfigRootDir: __dirname,
-    sourceType: "module",
-  },
-  plugins: ["@stylexjs"],
-  rules: {
-    "@stylexjs/valid-styles": ["error", {}],
-  },
+	root: true,
+	extends: ["@repo/eslint-config/react-internal.js"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "./tsconfig.lint.json",
+		tsconfigRootDir: __dirname,
+		sourceType: "module",
+	},
 };
