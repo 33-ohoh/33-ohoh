10e60c61c183dc19b62fbdd62dcf09a31fbd733e
minzyee <vvzmsqufvv@gmail.com>
2024-01-30 16:11:32 +0900

    Feat: 마이페이지 사이드 컨테이너 마크업

--- apps/web/app/mypage/page.tsx
+++ apps/web/app/mypage/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-// import { useState } from "react";
-import * as stylex from "@stylexjs/stylex";
+import { useState } from "react";
 import AsideContainer from "../../components/mypage/AsideContainer";
 import MainContainer from "../../components/mypage/MainContainer";
 
 const Page = () => {
   // TODO:: 방문자 수 증가 구현
-  // const [visiting, setVisiting] = useState<number>(122);
-  // const [totalVisiting, setTotalVisiting] = useState<number>(12240);
+  const [visiting, setVisiting] = useState<number>(122);
+  const [totalVisiting, setTotalVisiting] = useState<number>(12240);
 
   return (
-    <div {...stylex.props(styles.layout)}>
-      <h2 {...stylex.props(styles.pageTitle)}>마이페이지</h2>
-      <div {...stylex.props(styles.visitingStatus)}>
-        <strong>{/* Today <span>{visiting}</span> */}</strong>
+    <div className="">
+      <h2 className="text-[28px] font-bold mt-[172px] mb-[75px]">마이페이지</h2>
+      <div className="flex mb-extraSmall3">
+        <strong>
+          Today <span>{visiting}</span>
+        </strong>
         <div>⎪</div>
-        <strong>{/* Total <span>{totalVisiting}</span> */}</strong>
+        <strong>
+          Total <span>{totalVisiting}</span>
+        </strong>
       </div>
-      <div {...stylex.props(styles.containerLayout)}>
+      <div className="flex gap-[50px]">
         <AsideContainer />
         <MainContainer />
       </div>
@@ -27,27 +30,3 @@ const Page = () => {
 };
 
 export default Page;
-
-const styles = stylex.create({
-  layout: {
-    display: "flex",
-    flexDirection: "column",
-  },
-
-  containerLayout: {
-    display: "flex",
-    gap: "50px",
-  },
-
-  pageTitle: {
-    fontSize: "28px",
-    marginTop: "188px",
-    marginBottom: "75px",
-  },
-
-  visitingStatus: {
-    display: "flex",
-    fontSize: "18px",
-    marginBottom: "15px",
-  },
-});
