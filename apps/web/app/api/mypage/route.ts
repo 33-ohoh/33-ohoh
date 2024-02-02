21ec526cafd04b5ac2578ce9ed1973468e41a6ce
minzyee <vvzmsqufvv@gmail.com>
2024-02-02 11:51:21 +0900

    Feat: db로 부터 log 데이터 가져오기

--- apps/web/app/api/mypage/route.ts
+++ apps/web/app/api/mypage/route.ts
@@ -1,17 +1,23 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import PocketBase from "pocketbase";
 
 const pb = new PocketBase("http://13.209.16.46:8090");
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (!req.url) {
-    return res.status(400).send("URL not provided");
-  }
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
 
-  const resultList = await pb.collection("logs").getFullList({
-    sort: "created",
-    expand: "user",
-  });
-  return NextResponse.json(resultList);
+  const page = searchParams.get("page") || 0;
+  const perPage = searchParams.get("perPage") || 0;
+
+  try {
+    const resultList = await pb
+      .collection("logs")
+      .getList(Number(page), Number(perPage), {
+        sort: "created",
+        expand: "user",
+      });
+    return NextResponse.json(resultList);
+  } catch (error) {
+    console.error("에러 발생");
+  }
 }
--- apps/web/components/mypage/MainContainer.tsx
+++ apps/web/components/mypage/MainContainer.tsx
@@ -1,7 +1,26 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import LogCard from "../../../../packages/ui/src/LogCard";
+import { useState } from "react";
+import useSWR from "swr";
+import LogCard from "./LogCard";
+import Pagination from "./Pagination";
+
+interface LogCardProps {
+  id: string;
+  title: string;
+  thumbnail: string;
+  expand: {
+    user: {
+      name: string;
+      myJob: string;
+    };
+  };
+  content: string;
+  hitCount: number;
+  likeCount: number;
+  commentCount: number;
+  collectionId: string;
+}
 
 const MainContainer = () => {
   const favoriteLogChip = [
@@ -12,40 +31,37 @@ const MainContainer = () => {
   ];
 
   // 전체 게시글의 개수가 담겨있다.
-  const [logs, setLogs] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 6; // 한 페이지당 보여줄 로그 수
+  const [page, setPage] = useState<number>(1);
+  const [limit, setLimit] = useState<number>(6);
 
-  useEffect(() => {
-    fetchLogs(currentPage);
-  }, [currentPage]);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const key = `/api/mypage?page=${page}&perPage=${limit}`;
 
-  const fetchLogs = (page: number) => {
-    fetch(`api/mypage?page=${page}&pageSize=${pageSize}`)
-      .then((response) => response.json())
-      .then((result) => {
-        setLogs(result);
-      });
+  const { data, error, isLoading } = useSWR(key, fetcher, {
+    revalidateOnMount: true,
+    revalidateOnFocus: true,
+  });
+
+  const handlePrev = () => {
+    setPage((prev) => prev - 1);
+  };
+  const handleNext = () => {
+    setPage((prev) => prev + 1);
   };
 
-  const totalLogs = logs.length;
-  const totalPages = Math.ceil(totalLogs / pageSize);
+  const items = data?.items || [];
+  const totalItems = data?.totalItems;
+  console.log("aa", totalItems);
 
-  const startIndex = (currentPage - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-
-  const displayedLogs = logs.slice(startIndex, endIndex);
-
-  const pageNumbers = Array.from(
-    { length: totalPages },
-    (_, index) => index + 1,
-  );
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>에러 발생: {error}</div>;
 
   return (
     <main>
       {/* <h3 className=" text-[28px] font-bold mb-extraSmall5">대표로그</h3> */}
+
       <div className="flex gap-[10px] mb-small1">
-        {favoriteLogChip.map((logChip: any, index: any) => (
+        {favoriteLogChip.map((logChip: string, index: number) => (
           <button
             key={index}
             type="button"
@@ -56,40 +72,19 @@ const MainContainer = () => {
         ))}
       </div>
       <div className="flex flex-col justify-center items-center gap-[75px]">
-        <ul className="flex flex-wrap gap-[50px] w-[694px] h-[1006px]">
-          {displayedLogs.map((log: any) => {
+        <ul className="flex flex-wrap gap-[50px] w-[694px]">
+          {items.map((log: LogCardProps) => {
             return <LogCard key={log.id} log={log} />;
           })}
         </ul>
-        <ul className="flex ">
-          <li className="flex items-center gap-1 cursor-pointer">
-            <span>&lt;</span>
-            <button
-              onClick={() => setCurrentPage(currentPage - 1)}
-              disabled={currentPage === 1}
-            >
-              이전
-            </button>
-          </li>
-          {pageNumbers.map((pageNumber) => (
-            <li
-              key={pageNumber}
-              className={`flex px-[5px] cursor-pointer ${pageNumber === currentPage ? "text-neutral60" : "text-neutral30"}`}
-              onClick={() => setCurrentPage(pageNumber)}
-            >
-              {pageNumber}
-            </li>
-          ))}
-          <li className="flex items-center gap-1 cursor-pointer">
-            <button
-              onClick={() => setCurrentPage(currentPage + 1)}
-              disabled={currentPage === totalPages}
-            >
-              다음
-            </button>
-            <span>&gt;</span>
-          </li>
-        </ul>
+        <Pagination
+          handlePrev={handlePrev}
+          handleNext={handleNext}
+          totalItems={totalItems}
+          page={page}
+          limit={limit}
+          setPage={setPage}
+        />
       </div>
     </main>
   );
