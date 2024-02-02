5084f8be45a246bae6a9a0d0a8dd1673860cb968
Minji Kim <vvzmsqufvv@gmail.com>
2024-02-02 15:24:03 +0900

    [Update] IconComponents svg 컴포넌트 리팩토링 (#57)
    * Refactor: 아이콘 컴포넌트 리팩토링
    
    * Remove: 사용하지 않는 파일 삭제
    
    * Refactor: 아이콘 컴포넌트 등록
    
    * Rename: 폴더 이동

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,14 @@
 import MainPage from "./main/page";
+import { AttachAFile, Bookmark, HeartFull } from "@repo/ui/index";
 
 export default function Page() {
   return (
     <div>
       <MainPage />
+
+      <AttachAFile />
+      <HeartFull fill="green" stroke="green" />
+      <Bookmark stroke="green" />
     </div>
   );
 }
