909ccb1d770442a987b760224912684305aef303
minzyee <vvzmsqufvv@gmail.com>
2024-02-02 11:56:38 +0900

    Feat: 페이지네이션 컴포넌트 작업

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,7 +2,11 @@ import sharedConfig from "@repo/tailwind-config";
 import type { Config } from "tailwindcss";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ], // Or if using `src` directory: "./src/**/*.{js,ts,jsx,tsx,
   presets: [sharedConfig],
   prefix: "ui-",
 };
