554711c710b296ef9e4cf2df4e97780c72b6e27e
an seungji <ansj8777@gmail.com>
2024-02-02 17:11:21 +0900

    Fix: config 내부 content 코드 수정(복구) #62

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,11 +2,7 @@ import sharedConfig from "@repo/tailwind-config";
 import type { Config } from "tailwindcss";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ], // Or if using `src` directory: "./src/**/*.{js,ts,jsx,tsx,
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   presets: [sharedConfig],
   prefix: "ui-",
 };
