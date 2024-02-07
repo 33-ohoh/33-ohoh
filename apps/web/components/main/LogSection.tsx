efa7c61323ebebd73ffdeb175328a4db1aa34050
Hyen <94895962+Hyen636@users.noreply.github.com>
2024-02-07 21:34:44 +0900

    [Feat] 메인페이지 필터 및 정렬 기능 구현 #72 (#73)
    * Chore: pocketbase 설치
    
    * Remove: icon.tsx 삭제
    
    * Update: 아이콘 추가
    
    * Refactor: 메인페이지 핫 로그 코드 수정
    
    * Refactor: 메인페이지 필터테그 호버 오류 수정
    
    * Feat: 메인 페이지 로그 관련 컴포넌트 제작
    
    * Feat: 메인페이지 필터 및 정렬 기능 구현
    
    * Refactor: 메인페이지 태그 기능 수정

--- apps/web/components/main/LogSection.tsx
+++ apps/web/components/main/LogSection.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import FilterTagList from "./FilterTagList";
+import FilterSort from "./FilterSort";
+import LogCardList from "./LogCardList";
 
 const LogSection = () => {
+  const [filteredTags, setFilteredTags] = useState<string[]>([]);
+  const [selectedSort, setSelectedSort] = useState<string>("전체");
+  const handleTagClick = (clickedTagText: string) => {
+    if (clickedTagText === "개발 전체") {
+      setFilteredTags([]);
+    } else {
+      const isTagFiltered = filteredTags.includes(clickedTagText);
+      if (isTagFiltered) {
+        setFilteredTags(filteredTags.filter((tag) => tag !== clickedTagText));
+      } else {
+        setFilteredTags([...filteredTags, clickedTagText]);
+      }
+    }
+  };
+  const handleSortChange = (sort: string) => {
+    setSelectedSort(sort);
+  };
   return (
-    <div className="w-[1066px] flex flex-col gap-y-[0.94rem] mt-[4.69rem]">
-      <span className="display4">로그 분야</span>
-      <FilterTagList />
+    <div className="w-[full] flex flex-col items-center">
+      <div className="w-[1066px] flex flex-col gap-y-[0.94rem] mt-[4.69rem]">
+        <span className="display4">로그 분야</span>
+        <FilterTagList
+          filteredTags={filteredTags}
+          onTagClick={handleTagClick}
+        />
+        <div className="mt-[74px] mb-[25px]">
+          <FilterSort onSortChange={handleSortChange} />
+        </div>
+      </div>
+      <LogCardList filteredTags={filteredTags} selectedSort={selectedSort} />
     </div>
   );
 };
