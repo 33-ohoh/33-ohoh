28c75e82d94d0f567f8cd89d9d13bb8c66e4f9d6
wantop1 <wantop@jbnu.ac.kr>
2024-02-11 15:07:55 +0900

    Feat: 이름 오류 메시지 추가

--- apps/web/constants/errorMessages.ts
+++ apps/web/constants/errorMessages.ts
@@ -3,6 +3,8 @@ export const ERROR_MESSAGES = {
   INVALID_EMAIL_FORMAT: "올바른 이메일주소를 입력해주세요.",
   PASSWORD_REQUIRED: "비밀번호를 입력해주세요.",
   INVALID_PASSWORD_FORMAT: "비밀번호 형식이 올바르지 않습니다.",
+  NAME_REQUIRED: "이름을 입력해주세요.",
+  USERNAME_REQUIRED: "닉네임을 입력해주세요.",
   AUTHENTICATION_FAILURE_MESSAGE:
     "가입되어 있지 않은 계정이거나, 이메일 또는 비밀번호가 일치하지 않습니다.",
 };
