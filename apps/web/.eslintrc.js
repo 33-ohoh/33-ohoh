953163c47939f991e1d8febea924afdf696bf171
an seungji <ansj8777@gmail.com>
2024-02-14 15:58:47 +0900

    Chore: 빌드 과정중 에러 메시지 확인을위한 임시 eslint 규칙 비활성화

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -6,4 +6,8 @@ module.exports = {
   parserOptions: {
     project: true,
   },
+  rules: {
+    "no-unused-vars": "off", // 사용하지 않는 변수에 대한 경고 비활성화
+    "react/no-unescaped-entities": "off", // React에서 특수 문자가 escape 되지 않았을 때의 경고 비활성화
+  },
 };
